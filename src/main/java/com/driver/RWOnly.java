cff4316828249030b4c932028917dfd1fb60ad37
Harit <haritshandilya.beas@gmail.com>
2023-06-08 22:57:59 +0530

    "Completed assignment."

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int var1=0;
+
+    public int getVar1() {
+        return var1;
+    }
+
+    public void setVar1(int var1) {
+        this.var1 = var1;
+    }
 }
