e719288d9b96131dc9a4f1589247380d5959f4ea
Harit <haritshandilya.beas@gmail.com>
2023-06-08 23:15:00 +0530

    Final Submission

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
         System.out.println("Before changing the data : "+obj.getVar1());
-        obj.setVar1(50);
+        obj.setVar1(40);
         System.out.println("After changing the data : "+obj.getVar1());
         //'var1' has private access in 'com.driver.RWOnly'
     }
